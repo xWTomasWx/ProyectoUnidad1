8ce476b06d936193713f07b8a5bdd39d6f422fce
unknown <josetpffpt@gmail.com>
2023-10-09 03:05:32 -0300

    tercer commit

--- ProyectoUnidad1.c
+++ ProyectoUnidad1.c
@@ -13,6 +13,15 @@ typedef struct Carta
     struct Carta *siguiente;
 }Carta;
 
+typedef struct 
+{
+    int vida;
+    Carta *cabeceraCartasEnMano;
+    Carta *cabeceraCartasEnMesa;
+    Carta *cabeceraCartasEnMazo;
+}Jugador;
+
+
 void agregarEstructura(Carta **cabecera, Carta *nuevaCarta){
     if(*cabecera == NULL){
         *cabecera = nuevaCarta;
@@ -29,7 +38,7 @@ void inicializarListaTexto(Carta **cabecera){
     FILE *archivo = fopen("Guardianes.txt", "r");
     if(archivo == NULL){
         printf("Error al abrir el archivo\n");
-        return;
+        exit(-1);
     }
     
     int maxLineaLongitud = 300;
@@ -141,7 +150,10 @@ void crearCarta(Carta **cabecera){
     while(opcion > 4 || opcion < 1){
         printf("Seleccione un tipo para su carta:\n1) Mago\n2) Vikingo\n3) Nigromante\n4) Bestia\n");
         printf("Ingrese el numero: ");
-        scanf("%d", &opcion);
+        if(scanf("%d", &opcion) != 1){
+        	opcion = 5;
+        	while(getchar() != '\n');
+		}
         if(opcion == 1){
             strcpy(nuevaCarta->tipo, "Mage");
         }
@@ -166,13 +178,14 @@ void crearCarta(Carta **cabecera){
     printf("\nCarta creada con exito\n");
 }
 
-
 void imprimirLista(Carta *cabecera){
     Carta *actual = cabecera;
+    int contador = 1;
     while(actual != NULL){
-        printf("Nombre: %s, Tipo: %s, Vida: %d, Ataque: %d, Defensa: %d\n", 
+        printf("%d) Nombre: %s, Tipo: %s, Vida: %d, Ataque: %d, Defensa: %d\n", contador,
         actual->nombre, actual->tipo, actual->vida, actual->ataque, actual->defensa);
         actual = actual->siguiente;
+        contador++;
     }
 }
 
@@ -184,17 +197,141 @@ void limpiarLista(Carta *cabecera){
     }
 }
 
+int verificarCantidadCartas(Carta **cabecera){
+    Carta *actual = *cabecera;
+    int contador = 0;
+    while(actual != NULL){
+        contador++;
+        actual = actual->siguiente;
+    }
+    if(contador >= 30){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void inicializarJugador(Jugador *jugador){
+    jugador->vida = 5;
+    jugador->cabeceraCartasEnMano = NULL;
+    jugador->cabeceraCartasEnMazo = NULL;
+    jugador->cabeceraCartasEnMesa = NULL;
+}
+
+void agregarCartaMazo(Jugador *jugador, Carta *carta){
+    if(jugador->cabeceraCartasEnMazo == NULL){
+        jugador->cabeceraCartasEnMazo = carta;
+        return;
+    }
+    Carta *actual = jugador->cabeceraCartasEnMazo;
+    while(actual->siguiente != NULL){
+        actual = actual->siguiente;
+    }
+    actual->siguiente = carta;
+}
+
+void agregarCartasMazo(Jugador *usuario, Jugador *CPU, Carta **cabecera){
+    
+    
+    int i;
+    for(i=0; i<15; i++){
+    	Carta *actual = *cabecera;
+    	Carta *anterior = actual;
+        while(actual->siguiente != NULL){
+        	anterior = actual;
+        	actual = actual->siguiente;
+		}
+		anterior->siguiente = actual->siguiente;
+		actual->siguiente = NULL;
+		agregarCartaMazo(usuario, actual);
+		Carta *actual2 = *cabecera;
+    	Carta *anterior2 = actual2;
+        while(actual2->siguiente != NULL){
+        	anterior2 = actual2;
+        	actual2 = actual2->siguiente;
+		}
+		anterior2->siguiente = actual2->siguiente;
+		actual2->siguiente = NULL;
+		agregarCartaMazo(CPU, actual2);
+    }
+}
+
+void barajarMazo(Carta **cabecera) {
+    int cantidadCartas = 0;
+    Carta *actual = *cabecera;
+    while (actual != NULL) {
+        cantidadCartas++;
+        actual = actual->siguiente;
+    }
+
+    Carta *cartas[cantidadCartas];
+    actual = *cabecera;
+    int i = 0;
+    while (actual != NULL) {
+        cartas[i] = actual;
+        actual = actual->siguiente;
+        i++;
+    }
+
+    for (i = cantidadCartas - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        Carta *temp = cartas[i];
+        cartas[i] = cartas[j];
+        cartas[j] = temp;
+    }
+
+    *cabecera = cartas[0];
+    for (i = 0; i < cantidadCartas - 1; i++) {
+        cartas[i]->siguiente = cartas[i + 1];
+    }
+    cartas[cantidadCartas - 1]->siguiente = NULL;
+}
+
+void juegoCartas(Carta **cabecera){
+    if(verificarCantidadCartas(cabecera) == 0){
+        printf("\nNumero de cartas insuficientes para comenzar a jugar\n");
+        return;
+    }
+    
+    Jugador *usuario = (Jugador*)malloc(sizeof(Jugador));
+    Jugador *CPU = (Jugador*)malloc(sizeof(Jugador));
+    inicializarJugador(usuario);
+    inicializarJugador(CPU);
+    
+    barajarMazo(cabecera);
+	
+    agregarCartasMazo(usuario, CPU, cabecera);
+    
+    
+    imprimirLista(usuario->cabeceraCartasEnMazo);
+    printf("\n\n\n");
+    imprimirLista(CPU->cabeceraCartasEnMazo);
+	printf("\n\n\n");
+    imprimirLista(*cabecera);
+	
+	
+	limpiarLista(usuario->cabeceraCartasEnMano);
+	limpiarLista(usuario->cabeceraCartasEnMazo);
+	limpiarLista(usuario->cabeceraCartasEnMesa);
+	limpiarLista(CPU->cabeceraCartasEnMano);
+	limpiarLista(CPU->cabeceraCartasEnMazo);
+	limpiarLista(CPU->cabeceraCartasEnMesa);
+}
+
 void menu(Carta **cabecera){
     int opcion=0;
     while(opcion != 4){
         printf("Seleccione una opcion:\n1) Crear carta\n2) Jugar\n3) Ver historial\n4) Salir\n");
         printf("Ingrese el numero: ");
-        scanf("%d", &opcion);
+        if(scanf("%d", &opcion) != 1){
+        	opcion = 5;
+        	while(getchar() != '\n');
+		}
         if(opcion == 1){
             crearCarta(cabecera);
         }
         if(opcion == 2){
-            
+            juegoCartas(cabecera);
         }
         if(opcion == 3){
             
